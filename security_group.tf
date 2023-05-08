6f7c0a93f265dad6b080a3c25283fabe375afedd
hoanlac9 <hoan.lac@outlook.com>
2023-05-08 23:20:06 +0700

    init nsg

---
+++ security_group.tf
@@ -0,0 +1,31 @@
+module "sg_label" {
+  source     = "cloudposse/label/null"
+  version    = "0.25.0"
+  attributes = ["sg"]
+  context    = module.base_label.context
+}
+
+module "sg_database" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+
+  name        = "${module.sg_label.id}-database"
+  description = "Security group for Database"
+  vpc_id      = module.base_network.vpc_id
+
+  ingress_with_source_security_group_id = [
+
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      description = "Allow output to Local"
+      cidr_blocks = module.base_network.vpc_cidr_block
+    }
+  ]
+
+  tags = module.sg_label.tags
+}
