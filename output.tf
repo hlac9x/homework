3bce2b0353b3ff1fd002dc377fd25857daab517a
hoanlac9 <hoan.lac@outlook.com>
2023-05-09 15:05:22 +0700

    add ec2

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
-# ##  Wordpress Host ##
-# output "wordpress_public_ip" {
-#   description = "Public IP of the wordpress VM"
-#   value       = try(aws_eip.wordpress.public_ip, "")
-# }
+##  Wordpress Host ##
+output "wordpress_public_ip" {
+  description = "Public IP of the wordpress VM"
+  value       = try(aws_eip.wordpress.public_ip, "")
+}
